d6e787cb8d6f92988de803e3c4654a0e371cdec0
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-25 23:56:59 +0530

    app s file updated with routs

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Routes, Route} from "react-router-dom";
+import { themeSettings } from "theme";
+import Dashboard from "scenes/dashboard";
+
 function App() {
+  const mode = useSelector((state) => state.global.mode); // eslint-disable-line react-hooks/exhaustive-deps
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);  // eslint-disable-line react-hooks/exhaustive-deps 
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+            <Routes>
+              <Route element={<Layout />} > 
+                <Route path="/" element={<Navigate to="/dashboard" replace />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+              </Route>
+            </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
