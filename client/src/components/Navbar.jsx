cd5ee1f0ebbd89daac48997aaaa13ef3de465aa1
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-26 12:22:57 +0530

    Nav bar component iconeButton flex betwenn added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,13 +11,38 @@ import FlexBetween from './FlexBetween';
 import { useDispatch } from 'react-redux';
 import { setMode } from 'state';
 import profileImage from 'assets/profile.jpeg';
+import { AppBar, Icon, IconButton, InputBase, Toolbar, useTheme } from '@mui/material';
 
 const Navbar = () => {
-  return (
-    <div>
-      Navbar
-    </div>
-  )
+  const dispatch = useDispatch();
+  const theme = useTheme
+  return <AppBar position="static"
+    sx={{
+      position: "static",
+      background: "none",
+      boxShadow: "none",
+    }}
+  > 
+  <Toolbar sx={{ justifyContent: "space-between"}}> 
+  {/* LEFT SIDE */}
+  <FlexBetween>
+    <IconButton onClick={() => console.log('open/close sidebar')}>
+      <MenuIcon />
+    </IconButton>
+    <FlexBetween
+      backgroundColor={theme.palette.background.alt}
+      borderRadius="9px"
+      gap="3rem"
+      p="0.1rem 1.5rem"
+    >
+      <InputBase placeholder="Search..." />
+      <IconButton>
+        <Search />
+      </IconButton>
+    </FlexBetween>
+  </FlexBetween>
+  </Toolbar>
+  </AppBar>
 }
 
 export default Navbar
