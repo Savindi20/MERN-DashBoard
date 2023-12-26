a14ae232c569cd141da338cc4b5232a4d836a95f
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-26 11:35:23 +0530

    Nav bar component file added and updated

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { 
+  LightModeOutlined, 
+  DarkModeOutlined, 
+  Menu as MenuIcon, 
+  Search, 
+  SettingsOutlined, 
+  ArrowDropDownOutlined 
+} from '@mui/icons-material';
+import FlexBetween from './FlexBetween';
+import { useDispatch } from 'react-redux';
+import { setMode } from 'state';
+import profileImage from 'assets/profile.jpeg';
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
