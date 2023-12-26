c108e0390ef2afff03a6b1f0871e7a39cecde11f
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-26 13:27:07 +0530

    layout index file updated with nav bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -24,6 +24,7 @@ const Navbar = () => {
     }}
   > 
   <Toolbar sx={{ justifyContent: "space-between"}}> 
+
   {/* LEFT SIDE */}
   <FlexBetween>
     <IconButton onClick={() => console.log('open/close sidebar')}>
@@ -41,8 +42,22 @@ const Navbar = () => {
       </IconButton>
     </FlexBetween>
   </FlexBetween>
+
+  {/* RIGHT SIDE */}
+  <FlexBetween gap="1.5rem">
+    <IconButton onClick={() => dispatch(setMode())}>
+      {theme.palette.mode === 'dark' ? (
+        <DarkModeOutlined sx={{ fontSize: "25px"}}/>
+      ):(
+        <LightModeOutlined sx={{ fontSize: "25px"}}/>
+      )}
+    </IconButton>
+    <IconButton>
+      <SettingsOutlined sx={{ fontSize: "25px"}}/> 
+    </IconButton>
+  </FlexBetween>
   </Toolbar>
   </AppBar>
 }
 
-export default Navbar
+export default Navbar;
