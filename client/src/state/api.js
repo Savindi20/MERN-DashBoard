320a5eecdff675751742c0b161b190681263969e
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-28 16:53:40 +0530

    state api updated with getproduct query

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,23 +3,18 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: "adminApi",
-  tagTypes: [ "User"],
+  tagTypes: [ "User", "Products"],
 
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
+      providesTags: [ "User" ],
+    }),
+    getProducts: build.query({
+      query: () => `general/products`,
+      providesTags: [ "Products" ],
     }),
   }),
 });
 
-export const {
-    useGetUserQuery,
-    useGetProductsQuery,
-    useGetCustomersQuery,
-    useGetTransactionsQuery,
-    useGetGeographyQuery,
-    useGetSalesQuery,
-    useGetAdminsQuery,
-    useGetUserPerformanceQuery,
-    useGetDashboardQuery,
-  } = api;
+export const { useGetUserQuery, useGetProductsQuery } = api;
