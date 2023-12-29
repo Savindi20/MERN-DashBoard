cf63781080c4d0f444f3d4ed38344eb8583f3d8c
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-29 06:04:20 +0530

    header component file added and completed

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -1,24 +1,27 @@
-import React from "react";
-// import {
-//   Box,
-//   Card,
-//   CardActions,
-//   CardContent,
-//   Collapse,
-//   Button,
-//   Typography,
-//   Rating,
-//   useTheme,
-//   useMediaQuery,
-// } from "@mui/material";
-// import Header from "components/Header";
-// import { useGetProductsQuery } from "state/api";
+import React, { useState } from "react";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Collapse,
+  Button,
+  Typography,
+  Rating,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import Header from "components/Header";
+import { useGetProductsQuery } from "state/api";
 
-const index = () => {
+const Products = () => {
+    const { data, isloading} = useGetProductsQuery();
+    const isNonMobile = useMediaQuery("min-width: 1000px");
+    console.log("data", data);
   return (
-    <div>
-      
-    </div>
+    <Box m="1.5rem 2.">
+      <Header title="PRODUCTS" subtitle="Check out our products" /> 
+    </Box>
   )
 }
 
