35e81a5e8fe9e76b7301947799694467ccf928fd
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-29 17:02:22 +0530

    index js file and client rout updated with transaction

--- server/index.js
+++ server/index.js
@@ -14,10 +14,12 @@ import salesRoutes from "./routes/sales.js";
 import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
 import {
   dataUser,
   dataProduct,
   dataProductStat,
+  dataTransaction,
 } from "./data/index.js";
 
 /* CONFIGURATION */
@@ -50,6 +52,7 @@ mongoose
     /* ONLY ADD DATA ONE TIME */
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
     // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
