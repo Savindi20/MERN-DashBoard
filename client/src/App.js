ec40976d64be58d9307d8349e5651a6b799daec2
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-16 20:17:12 +0530

    breakdown file added and completed

--- client/src/App.js
+++ client/src/App.js
@@ -14,8 +14,6 @@ import Overview from "scenes/overview";
 import Daily from "scenes/daily";
 import Monthly from "scenes/monthly";
 import Breakdown from "scenes/breakdown";
-import Admin from "scenes/admin";
-import Performance from "scenes/performance";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -35,6 +33,8 @@ function App() {
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
+              <Route path="/breakdown" element={<Breakdown />} />
             </Route>
           </Routes>
         </ThemeProvider>
