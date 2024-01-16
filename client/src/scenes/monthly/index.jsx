18bfe7d6296976793f751d29f18188ceb681fc12
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-16 14:28:37 +0530

    monthly chart index file added

---
+++ client/src/scenes/monthly/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box } from "@mui/material";
+import Header from "components/Header";
+
+const Monthly = () => {
+  
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="MONTHLY SALES" subtitle="Chart of monthlysales" />
+    </Box>
+  );
+};
+
+export default Monthly;
