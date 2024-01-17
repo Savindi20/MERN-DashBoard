67c2ef74bc3d8f888aec49eb3ca7fbe5f7fed551
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-18 01:03:43 +0530

    performannce scennes file added

---
+++ client/src/scenes/performance/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box } from "@mui/material";
+import Header from "components/Header";
+
+const Performance = () => {
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header
+        title="PERFORMANCE"
+        subtitle="Track your Affiliate Sales Performance Here"
+      />
+    </Box>
+  );
+};
+
+export default Performance;
