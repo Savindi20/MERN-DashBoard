62fc08c76a6ddc77c8f42d0d7011b749c3ed2261
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-17 20:04:12 +0530

    admin scenes file added

---
+++ client/src/scenes/admin/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box } from "@mui/material";
+import Header from "components/Header";
+
+const Admin = () => {
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="ADMINS" subtitle="Managing admins and list of admins" />
+    </Box>
+  );
+};
+
+export default Admin;
