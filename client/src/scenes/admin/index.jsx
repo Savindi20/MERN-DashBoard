921cc1c92eff8777c0c8bdaae6347f2cf0bc3db7
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-17 21:15:05 +0530

    admin column added

--- client/src/scenes/admin/index.jsx
+++ client/src/scenes/admin/index.jsx
@@ -1,8 +1,52 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
+import { useGetAdminsQuery } from "state/api";
 import Header from "components/Header";
 
 const Admin = () => {
+  const theme = useTheme();
+  const { data, isLoading } = useGetAdminsQuery();
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 0.5,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "phoneNumber",
+      headerName: "Phone Number",
+      flex: 0.5,
+      renderCell: (params) => {
+        return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3");
+      },
+    },
+    {
+      field: "country",
+      headerName: "Country",
+      flex: 0.4,
+    },
+    {
+      field: "occupation",
+      headerName: "Occupation",
+      flex: 1,
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      flex: 0.5,
+    },
+  ];
 
   return (
     <Box m="1.5rem 2.5rem">
